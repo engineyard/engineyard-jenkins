263d6e54497ae0e4a1b8480217b6cd7de54ae884
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-28 22:24:00 -0700

    initial wiring of 'server' task

---
+++ features/step_definitions/hudson_steps.rb
@@ -0,0 +1,3 @@
+Given /^I have an environment "([^"]*)" on account "([^"]*)" on AppCloud$/ do |env, account|
+
+end
--- lib/engineyard-hudson/cli.rb
+++ lib/engineyard-hudson/cli.rb
@@ -5,6 +5,15 @@ module Engineyard
     class CLI < Thor
       map "-v" => :version, "--version" => :version, "-h" => :help, "--help" => :help
       
+      desc "server", "Setup a Hudson CI server on AppCloud"
+      method_option :environment, :type => :string, :aliases => %w(-e),
+        :desc => "Environment in which to deploy this application", :required => true
+      method_option :account, :type => :string, :aliases => %w(-c),
+        :desc => "Name of the account you want to deploy in"
+      def server
+        
+      end
+      
       desc "version", "show version information"
       def version
         shell.say Engineyard::Hudson::VERSION
