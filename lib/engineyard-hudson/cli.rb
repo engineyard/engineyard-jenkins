6446617d8777703ebdbc4c3181e2d8c0ef8e7351
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-28 22:19:54 -0700

    add a 'version' task

--- lib/engineyard-hudson/cli.rb
+++ lib/engineyard-hudson/cli.rb
@@ -3,8 +3,12 @@ require 'thor'
 module Engineyard
   module Hudson
     class CLI < Thor
-
       map "-v" => :version, "--version" => :version, "-h" => :help, "--help" => :help
+      
+      desc "version", "show version information"
+      def version
+        shell.say Engineyard::Hudson::VERSION
+      end
     end
   end
 end
