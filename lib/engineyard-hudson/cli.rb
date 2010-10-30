81aaf4ae92900fa13114176a27fe523db3f3f428
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-29 21:54:39 -0700

    refactoring of cli.rb layout

--- lib/engineyard-hudson/cli.rb
+++ lib/engineyard-hudson/cli.rb
@@ -3,22 +3,23 @@ require 'thor'
 module Engineyard
   module Hudson
     class CLI < Thor
-      map "-v" => :version, "--version" => :version, "-h" => :help, "--help" => :help
+      include Thor::Actions
+      
+      def self.common_options
+        method_option :environment, :type => :string, :aliases => %w(-e), 
+          :desc => "Environment in which to deploy this application", :required => true
+        method_option :account, :type => :string, :aliases => %w(-c), 
+          :desc => "Name of the account you want to deploy in"
+      end
       
       desc "install PROJECT_PATH", "Install Hudson node/slave recipes into your project."
-      method_option :environment, :type => :string, :aliases => %w(-e),
-        :desc => "Environment in which to deploy this application", :required => true
-      method_option :account, :type => :string, :aliases => %w(-c),
-        :desc => "Name of the account you want to deploy in"
+      common_options
       def install(project_path)
         shell.say "Coming soon!", :green
       end
       
       desc "server", "Setup a Hudson CI server on AppCloud."
-      method_option :environment, :type => :string, :aliases => %w(-e),
-        :desc => "Environment in which to deploy this application", :required => true
-      method_option :account, :type => :string, :aliases => %w(-c),
-        :desc => "Name of the account you want to deploy in"
+      common_options
       def server
         shell.say "Coming soon!", :green
       end
@@ -27,6 +28,8 @@ module Engineyard
       def version
         shell.say Engineyard::Hudson::VERSION
       end
+
+      map "-v" => :version, "--version" => :version, "-h" => :help, "--help" => :help
 
       private
       def display(text)
