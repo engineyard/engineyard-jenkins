7eb9cd0a4b9f2b5cf9df1f8af12dd8f895de3e14
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-30 14:53:34 -0700

    hudson_slave chef recipe should only create new jobs; and ignore existing jobs/applications

--- lib/engineyard-hudson/cli/install/templates/recipes.rb
+++ lib/engineyard-hudson/cli/install/templates/recipes.rb
@@ -54,38 +54,30 @@ if ['solo','app_master'].include?(node[:instance_role]) && env_name =~ /_(ci|hud
     action :create
   end
 
-  # ey_cloud_report "hudson-slave-setup" do
-  #   message "Added instance to Hudson CI server"
-  # end
-  
-  ruby_block "tell-master-about-job" do
+  ruby_block "tell-master-about-new-jobs" do
     block do
-      node[:hudson_slave][:applications] ||= []
+      begin
+        job_names   = Hudson::Api.job_names
+        app_names   = node[:applications].keys
+        apps_to_add = app_names - job_names
 
-      # Tell server about each application
-      node[:applications].each do |app_name, data|
+        # Tell server about each application
+        apps_to_add.each do |app_name|
+          data = node[:applications][app_name]
 
-        job_config = Hudson::JobConfigBuilder.new(:rails) do |c|
-          c.scm           = data[:repository_name]
-          c.assigned_node = app_name
-        end
-
-        if Hudson::Api.create_job(app_name, job_config, :override => true)
-          build_url = "#{Hudson::Api.base_uri}/job/#{app_name}/build"
-          node[:hudson_slave][:applications] << { :name => app_name, :success => true, :build_url => build_url }
+          job_config = Hudson::JobConfigBuilder.new("rails") do |c|
+            c.scm           = data[:repository_name]
+            c.assigned_node = app_name
+          end
+        
+          Hudson::Api.create_job(app_name, job_config)
           Hudson::Api.build_job(app_name)
-        else
-          node[:hudson_slave][:applications] << { :name => app_name, :success => false }
         end
+      rescue Errno::ECONNREFUSED, Errno::EAFNOSUPPORT
+        raise Exception, "No connection available to the Hudson server (#{Hudson::Api.base_uri})."
       end
     end
     action :create
   end
-
-  # ey_cloud_report "hudson-jobs-setup" do
-  #   node[:hudson_slave][:applications].each do |app|
-  #     message "Setup build trigger to #{app[:build_url]}"
-  #   end
-  # end
 
 end
