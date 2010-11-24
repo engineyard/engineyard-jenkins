495a65e1248fa6ffd9bacbd250fb94966e819cb7
Dr Nic Williams <drnicwilliams@gmail.com>
2010-11-23 21:43:43 -0800

    More specs for .find_environments with :account & :environment arguments

--- spec/appcloud_env_spec.rb
+++ spec/appcloud_env_spec.rb
@@ -42,4 +42,33 @@ describe Engineyard::Hudson::AppcloudEnv do
       find_environments.should == [['hudson', 'mine'], ['hudson_production', 'mine'], ['hudson_production', 'yours']]
     end
   end
+
+  describe ".find_environments - specific account" do
+    it "return [nil, nil] unless it has reason to return something else" do
+      appcloud_env.stub(:fetch_environment).and_raise(EY::NoEnvironmentError)
+      find_environments(:account => "mine").should == []
+    end
+    it "returns [env_name, account_name] if finds one env 'hudson' in specific account" do
+      appcloud_env.should_receive(:fetch_environment).with("hudson", "mine").and_return(EY::Model::App.new(123, EY::Model::Account.new(789, 'mine')))
+      appcloud_env.should_receive(:fetch_environment).with("hudson_server", "mine").and_raise(EY::NoEnvironmentError)
+      appcloud_env.should_receive(:fetch_environment).with("hudson_production", "mine").and_raise(EY::NoEnvironmentError)
+      appcloud_env.should_receive(:fetch_environment).with("hudson_server_production", "mine").and_raise(EY::NoEnvironmentError)
+      find_environments(:account => "mine").should == [['hudson', 'mine']]
+    end
+  end
+
+  describe ".find_environments - specific environment" do
+    it "return [nil, nil] unless it has reason to return something else" do
+      appcloud_env.stub(:fetch_environment).and_raise(EY::NoEnvironmentError)
+      find_environments(:environment => "hudson").should == []
+    end
+    it "returns [env_name, account_name] if finds one env 'hudson' in any account" do
+      appcloud_env.should_receive(:fetch_environment).with("hudson", nil).and_return(EY::Model::App.new(123, EY::Model::Account.new(789, 'mine')))
+      find_environments(:environment => "hudson").should == [['hudson', 'mine']]
+    end
+    it "returns [env_name, account_name] if finds one env 'hudson' in specific account" do
+      appcloud_env.should_receive(:fetch_environment).with("hudson", "mine").and_return(EY::Model::App.new(123, EY::Model::Account.new(789, 'mine')))
+      find_environments(:environment => "hudson", :account => "mine").should == [['hudson', 'mine']]
+    end
+  end
 end
